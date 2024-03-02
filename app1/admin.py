b4489031aa0b8142727232342850a665980f3a95
loharprathmesh <loharprathmesh123@gmail.com>
2024-03-02 12:46:48 +0530

    all done

--- app1/admin.py
+++ app1/admin.py
@@ -4,17 +4,27 @@ from .models import tanks,marker
 
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
+# Register your models here.
 
 # Register your models here.
 
 
-@admin.register(tanks)
-class tanks(ImportExportModelAdmin):
+@admin.register(marker)
+class markerAdmin(ImportExportModelAdmin):
     pass
 
-# class tankResource(resources.ModelResource):
 
-#     class Meta:
-#         model = tanks
+@admin.register(tanks)
+class tanksAdmin(ImportExportModelAdmin):
+    pass
 
-admin.site.register(marker)
+# @admin.register(tanks)
+# class tanks(ImportExportModelAdmin):
+#     pass
+
+# # class tankResource(resources.ModelResource):
+
+# #     class Meta:
+# #         model = tanks
+
+# admin.site.register(marker)
--- valveman/settings.py
+++ valveman/settings.py
@@ -123,9 +123,18 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static") 
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static") 
+# ]
+
+if DEBUG:
+
+  STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+else:
+
+  STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 
 # Default primary key field type
